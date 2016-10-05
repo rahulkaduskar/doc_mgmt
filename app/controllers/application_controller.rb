71799f4773f03121bcacf434891ad7b9e47d1441
Rahul Kaduskar <rahulk@Rahuls-MacBook-Pro.local>
2016-10-05 12:06:28 +0530

    login related issue fixed.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
+  include ActionController::Cookies
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
-  before_action :authenticate_current_user
+  before_action :authenticate_current_user, unless: :devise_controller?
 
   rescue_from ActionController::InvalidAuthenticityToken do |exception|
     cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
